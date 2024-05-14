24e6a510fd7cfd367fe2acf96c043845dedb9f1c
Julya Shapaeva <shapaeva03@mail.ru>
2024-05-15 00:54:01 +0300

    Delete classes

--- src/classes/abstract_student.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class AbstractStudent(ABC):
-    @abstractmethod
-    def __init__(self, id: int, first_name: str, last_name: str, patronymic: str,
-                 group: str, registration_date: str, team_id: int):
-        pass
