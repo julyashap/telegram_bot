bdeef2b081eecf4e74ba8ad642d1695e974557e3
Julya Shapaeva <shapaeva03@mail.ru>
2024-05-15 02:34:57 +0300

    Write tests for DBConnector

--- tests/test_db_connector.py
+++ tests/test_db_connector.py
@@ -2,40 +2,57 @@ import pytest
 from src.classes.db_connector import DBConnector
 
 
-# Фикстура для подготовки тестовой среды
 @pytest.fixture
 def db_connector():
-    connector = DBConnector("test_db_name")  # Подставьте имя вашей тестовой базы данных
+    connector = DBConnector("dance_icmit")
     yield connector
-    connector.conn.close()  # Закрыть соединение после завершения тестов
+    connector.conn.close()
 
 
-# Тесты
 def test_get_data(db_connector):
-    query = "SELECT * FROM some_table"
-    result = db_connector.get_data(query)
-    assert isinstance(result, list)
+    query = "select distinct team_name from teams"
+    results = db_connector.get_data(query)
+
+    assert isinstance(results, list)
+
+    for result in results:
+        assert isinstance(result, tuple)
 
 
 def test_insert_student(db_connector):
     student = {
-        "id": 1,
-        "first_name": "John",
-        "last_name": "Doe",
-        # ... дополнительные данные студента ...
+        "id": 123,
+        "first_name": "Юлия",
+        "last_name": "Шапаева",
+        "patronymic": "Александровна",
+        "group": "09-112",
+        "team_name": "Чердак",
+        "registration_date": "2024-05-15 19:00:00",
+        "selection_location": "УНИКС, спортивный блок: Центр Карпова",
+        "test_score": 4,
+        "team_id": 1
     }
     db_connector.insert_student(student)
-    # Проверить, что студент был добавлен успешно
+
+    student_ids = [student_id[0] for student_id in db_connector.get_data('select student_id from registrations')]
+
+    assert student['id'] in student_ids
 
 
 def test_delete_student(db_connector):
-    registration_id = 123  # Подставьте существующий registration_id для теста
+    registration_id = 1
     db_connector.delete_student(registration_id)
-    # Проверить, что студент был удален успешно
+
+    data = db_connector.get_data(f'select * from registrations where registration_id = {registration_id}')
+
+    assert str(data) == '[]'
 
 
 def test_update_code(db_connector):
-    chat_id = 456  # Подставьте значение для теста
-    supervisor_id = 789  # Подставьте значение для теста
+    chat_id = 456
+    supervisor_id = 1
     db_connector.update_code(chat_id, supervisor_id)
-    # Проверить, что код был успешно обновлен
+
+    data = db_connector.get_data(f'select chat_id from leaders where leader_id = {supervisor_id}')[0][0]
+
+    assert data == chat_id
